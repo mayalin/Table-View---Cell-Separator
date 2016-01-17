db7b9bae906e21e04118c427d08a8f1b433563f0
Yalin <mayalin2012@gmail.com>
2016-01-17 17:11:10 +0800

    Initial Commit

---
+++ Table View - Cell Separator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Table View - Cell Separator
+//
+//  Created by yalin.ma on 1/17/16.
+//  Copyright © 2016 yalin.ma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Table View - Cell SeparatorTests/Table_View___Cell_SeparatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Table_View___Cell_SeparatorTests.swift
+//  Table View - Cell SeparatorTests
+//
+//  Created by yalin.ma on 1/17/16.
+//  Copyright © 2016 yalin.ma. All rights reserved.
+//
+
+import XCTest
+@testable import Table_View___Cell_Separator
+
+class Table_View___Cell_SeparatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
